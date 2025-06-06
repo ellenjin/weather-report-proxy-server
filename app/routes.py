a36d9ece7fbf51bf3180ac93f19236df628e665f
Solhee <ellen.solhee.jin@gmail.com>
2025-06-06 13:29:30 -0700

    edited to work with proxy instead of local .env

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 bp = Blueprint("proxy_bp", __name__)
 
-location_key = os.environ.get("LOCATION_KEY")
-weather_key = os.environ.get("WEATHER_KEY")
+# location_key = os.environ.get("LOCATION_KEY")
+# weather_key = os.environ.get("WEATHER_KEY")
 
 @bp.get("/location")
 def get_lat_lon():
@@ -17,9 +17,13 @@ def get_lat_lon():
     if not loc_query:
         return {"message": "must provide q parameter (location)"}, 400
 
+    # response = requests.get(
+    #     "https://us1.locationiq.com/v1/search.php",
+    #     params={"q": loc_query, "key": location_key, "format": "json"}
+    # )
     response = requests.get(
-        "https://us1.locationiq.com/v1/search.php",
-        params={"q": loc_query, "key": location_key, "format": "json"}
+        "https://ada-weather-report-proxy-server.onrender.com/location",
+        params={"q": loc_query}
     )
 
     return response.json(), 200
@@ -32,9 +36,14 @@ def get_weather():
     if not lat_query or not lon_query:
         return {"message": "must provide lat and lon parameters"}, 400
 
+    # response = requests.get(
+    #     "https://api.openweathermap.org/data/2.5/weather",
+    #     params={"lat": lat_query, "lon": lon_query, "appid": weather_key}
+    # )
     response = requests.get(
-        "https://api.openweathermap.org/data/2.5/weather",
-        params={"lat": lat_query, "lon": lon_query, "appid": weather_key}
+        "https://ada-weather-report-proxy-server.onrender.com/weather",
+        params={"lat": lat_query, "lon": lon_query}
     )
+
     return response.json(), 200
 
